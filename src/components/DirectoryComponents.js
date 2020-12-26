a163f3513f3bf764e3f4b73d3604c9c71ff6f546
Hector L. Santiago <hlsp@me.com>
2020-12-26 08:17:00 -0600

    Checking the code in DirectoryComponenst.js.

--- src/components/DirectoryComponents.js
+++ src/components/DirectoryComponents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Directory extends Component {
     constructor(props) {
@@ -8,28 +9,51 @@ class Directory extends Component {
         };
     }
 
+    onCampsiteSelect(campsite) {
+        this.setState({selectedCampsite: campsite});
+    }
+
+    renderSelectedCampsite(campsite) {
+        if (campsite) {
+            return (
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />;
+    }
+
     render() {
         const directory = this.props.campsites.map(campsite => {
-            return(
-                <div key={campsite.id} className="col">
-                    <img src={campsite.image} alt={campsite.name} />
-                    <h2>{campsite.name}</h2>
-                    <p>{campsite.description}</p>
+            return (
+                <div key={campsite.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{campsite.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
-            )
-        })
-
+            );
+        });
 
         return (
             <div className="container">
                 <div className="row">
                     {directory}
-                </div>  
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
+                    </div>
+                </div>
             </div>
-                
-                
-
-        )
+        );
     }
 }
 
