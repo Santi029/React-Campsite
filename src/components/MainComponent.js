4fc9d18b14946373d4e4e34472c50929b332e338
Hector L. Santiago <hlsp@me.com>
2021-01-09 11:10:51 -0600

    Repairing this.state on maincomponent.js.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -25,7 +25,7 @@ class Main extends Component {
       const HomePage = () => {
           return(
               <Home
-                campsite = {this.state.campsites.filter(campsite => campsite.featured)[0]}
+                campsite = {this.props.campsites.filter(campsite => campsite.featured)[0]}
                 promotion = {this.props.promotions.filter(promotion => promotion.featured)[0]}
                 partner = {this.props.partners.filter(partner => partner.featured)[0]}
               />
