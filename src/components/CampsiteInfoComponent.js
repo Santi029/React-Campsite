b74a41e8c17e8846448865b9c1aa7067dc6fcf7f
Hector L. Santiago <hlsp@me.com>
2021-01-09 12:08:57 -0600

    Last changes for modal on the directory.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -11,12 +11,53 @@ import {
 import { Link } from "react-router-dom";
 
 class CommentForm extends Component {
-  render() {
-    return (
-      <Button className="fa fa-lg fa-pencil" outline>
-        Submit Comment
-      </Button>
-    );
+
+  constructor(props) {
+    super(props) {
+      this.state= {
+        isModalOpen: false
+      };
+
+      this.toggleModal = this.toggleModal.blind(this);
+    }
+    this.toggleModal() {
+      this.setState({
+        isModalOpen: !this.state.isModalOpen
+      });
+    }
+    
+    render() {
+      return (
+        <React.Fragment>
+          <Button className="fa fa-lg fa-pencil" outline> Submit Comment</Button>
+          <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
+            <ModalBody>
+              <Form onSubmit={this.handleLogin}>
+                <FormGroup>
+                    <Label htmlFor="username">Username</Label>
+                    <Input type="text" id="username" name="username"
+                        innerRef={input => this.username = input} />
+                </FormGroup>
+                <FormGroup>
+                    <Label htmlFor="password">Password</Label>
+                    <Input type="password" id="password" name="password"
+                        innerRef={input => this.password = input} />
+                </FormGroup>
+                <FormGroup check>
+                    <Label check>
+                        <Input type="checkbox" name="remember"
+                            innerRef={input => this.remember = input} />
+                        Remember me
+                    </Label>
+                </FormGroup>
+                <Button type="submit" value="submit" color="primary">Login</Button>
+              </Form>
+            </ModalBody>
+          </Modal>
+        </React.Fragment>
+      );
+    }
   }
 }
 
