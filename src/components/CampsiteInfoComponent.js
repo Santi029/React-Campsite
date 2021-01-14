fcd6e7c35a779144fccd5964cc7701d184f816b8
Hector L. Santiago <hlsp@me.com>
2021-01-13 22:44:10 -0600

    Updated the submit comment.

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -35,7 +35,7 @@ function RenderCampsite({ campsite }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -52,7 +52,7 @@ function RenderComments({ comments }) {
             }).format(new Date(Date.parse(comment.date)))}
           </div>
         ))}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -76,7 +76,11 @@ function CampsiteInfo(props) {
         </div>
         <div className="row">
           <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <RenderComments 
+            comments={props.comments}
+            addComment={props.addComment}
+            campsiteId={props.campsite.Id}
+          />
         </div>
       </div>
     );
@@ -104,8 +108,8 @@ class CommentForm extends Component {
   };
 
   handleSubmit(values) {
-    console.log("Your info is" + JSON.stringify(values));
-    alert("Your info is " + JSON.stringify(values));
+    // this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
 
   render() {
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,8 @@ import Contact from './ContactComponent';
 import About from './AboutusComponent';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
     return {
@@ -16,6 +18,10 @@ const mapStateToProps = state => {
         partners: state.partners,
         promotions: state.promotions
     };
+};
+
+const mapDispatchToProps = {
+    addComment:(campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text))
 };
 
 class Main extends Component {
@@ -37,6 +43,8 @@ class Main extends Component {
               <CampsiteInfo 
                 campsite = {this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
                 comments = {this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                addComment={this.props.addComment}
+
             />
           );
       }
@@ -60,4 +68,4 @@ class Main extends Component {
 
 
 
-export default withRouter (connect(mapStateToProps)(Main));
+export default withRouter (connect(mapStateToProps, mapDispatchToProps)(Main));
