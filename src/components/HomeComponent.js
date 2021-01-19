9b7a6c91b4f3eec1a386c229c24f5c153d6e5aec
Hector L. Santiago <hlsp@me.com>
2021-01-19 15:41:42 -0600

    Updating fetch.

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -33,13 +33,18 @@ function Home(props) {
                     />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} 
-                    isLoading={props.promotionLoading}
-                    errMess={props.promotionErrMess}
+                    <RenderCard 
+                        item={props.promotion} 
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
                     />
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.partner} />
+                    <RenderCard 
+                        item={props.partner} 
+                        isLoading={props.promotionLoading}
+                        errMess={props.promotionErrMess}
+                    />
                 </div>
             </div>
         </div>
